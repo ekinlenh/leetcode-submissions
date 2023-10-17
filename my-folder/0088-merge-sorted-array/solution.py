3e241dcb7641f70f32098f9993f43b2d8cea9034
ekinlenh <152040574+ekinlenh@users.noreply.github.com>
2023-10-17 03:50:53 +0000

    [LeetCode Sync] Runtime - 18 ms (3.14%), Memory - 13.3 MB (0.76%)

---
+++ my-folder/0088-merge-sorted-array/solution.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        for num in range(n):
+            nums1[m+num] = nums2[num]
+        nums1.sort()
+
